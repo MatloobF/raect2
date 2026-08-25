ce0c420c8efa9c6f97c47aaeb354416b5e703bad
MatloobF <farooquimatloob9@gmail.com>
2026-08-25 07:45:00 +0530

    files deleted

---
+++ reactP/projectvite/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+ 
+  return (
+    <>
+    <h1>chai aur reach 2</h1>
+    <Chai />
+    </>
+    
+  )
+    
+  
+}
+
+export default App
---
+++ reactP/projectvite/src/chai.jsx
@@ -0,0 +1,12 @@
+ 
+
+ function Chai() {
+    return (
+        <>
+            <h1>chai render from created app</h1>
+            <h3>aise hi</h3>
+        </>
+    )
+ }
+
+ export default Chai
